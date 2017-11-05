3938ca831315f06c5bbea4db83381620985dee55
Qian Hu <everawyd234@gmail.com>
2017-11-06 00:49:00 +0800

    change structure

---
+++ 2017MOOC-PTA/07-4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  07-图4 哈利·波特的考试  https://pintia.cn/problem-sets/900290821590183936/problems/919158178544488448
+//
+//  Created by Qian Hu on 2017/11/5.
+//  Copyright © 2017年 Qian Hu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
